3a95c42866a8d9f44bcabed4c4b8d8b590f2303e
Vishwas2Brud <vishwas.g@brudite.com>
2023-05-29 17:15:36 +0530

    commited

---
+++ Day_2/sum_subarray_0.py
@@ -0,0 +1,12 @@
+n = int(input("ENTER THE SIZE OF ARRAY \n"))
+arr = list(map(int,input().split()))
+sub_array = []
+
+for i in range(0,n+1):
+    for j in range(i+1,n+1):
+        sub_array.append(arr [i : j])
+    # print(sub_array)
+for i in sub_array:
+    if sum(i) == 0:
+        print(i)
+   
